864bd67602f030b498cf67d1373b913f897c80a4
RiyankB807032 <807032@pdsb.net>
2024-10-10 13:42:33 -0400

    Update Circumference.java

--- Circumference.java
+++ Circumference.java
@@ -1,10 +1,10 @@
   class Circumference {
-  public static void main(String [] args) {
-    // place your code here!
-    double circ;
-    double rad = 0.5;
-    circ = 2 * Math.PI * rad;
-    System.out.println("The circumference of a circle with the radius of " + rad + " equals " + circ );
-    // your code ends here!
-  }
-}
+    public static void main(String [] args) {
+      // place your code here!
+      double circ;
+      double rad = 0.5;
+      circ = 2 * Math.PI * rad;
+      System.out.println("The circumference of a circle with the radius of " + rad + " equals " + circ );
+      // your code ends here!
+      }
+    }
