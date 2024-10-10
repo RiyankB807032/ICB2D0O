90500a0138896af18edf7e53175ddf7f27ebafe3
RiyankB807032 <807032@pdsb.net>
2024-10-10 13:29:05 -0400

    Update Name.java

--- Name.java
+++ Name.java
@@ -1,2 +1,8 @@
-String name = "Riyank";
-System.out.println("Hello my man " + name);
+class Name { 
+  public static void main(String [] args) {
+    // place your code here!
+    String name = "Riyank";
+    System.out.println("Hello my man " + name);
+    // your code ends here!
+  }
+}
