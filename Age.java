3a25c6b7b9536e64d6c1be16c1923a7b21d69cd1
RiyankB807032 <807032@pdsb.net>
2024-10-10 13:42:06 -0400

    Update Age.java

--- Age.java
+++ Age.java
@@ -1,21 +1,21 @@
 class Testing {
 import java.util.Scanner;
 public class Main {
-public static void main(String[] args) {
-        // Your code goes here!
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your year of birth: ");
-        int birthYear = scanner.nextInt();
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-        int result = birthYear * 2;
-        result += 5;
-        result *= 50;
-        result += age;
-        result -= 250;
-        double finalResult = result / 100.0;
-        System.out.println("The result of the calculation is: " + finalResult);
-        System.out.println("This result is the combination of your birth year and age after performing the operations.");
-        // your code enhds here!
-        }
-      }
+        public static void main(String[] args) {
+                // Your code goes here!
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Enter your year of birth: ");
+                int birthYear = scanner.nextInt();
+                System.out.print("Enter your age: ");
+                int age = scanner.nextInt();
+                int result = birthYear * 2;
+                result += 5;
+                result *= 50;
+                result += age;
+                result -= 250;
+                double finalResult = result / 100.0;
+                System.out.println("The result of the calculation is: " + finalResult);
+                System.out.println("This result is the combination of your birth year and age after performing the operations.");
+                // your code enhds here!
+                }
+               }
