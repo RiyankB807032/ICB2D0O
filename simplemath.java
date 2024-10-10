5a5f34445f9a3e7444591fa94fc29572266943a5
RiyankB807032 <807032@pdsb.net>
2024-10-10 13:31:38 -0400

    Update and rename simple math.java to simplemath.java

---
+++ simplemath.java
@@ -0,0 +1,15 @@
+class simplemath { 
+        public static void main(String [] args) { 
+                // place your code here!
+                int x = 24;
+                int z = 7 * 4;
+                int a = 8;
+                int b = 3;
+                int amount = 8;
+                amount = amount + 1;
+                System.out.println(24);
+                System.out.println("Two dozen is " + x);
+                System.out.println("forty-two is " +z);
+                System.out.println(amount);
+                System.out.println(a*b); 
+                // your code ends here! 
