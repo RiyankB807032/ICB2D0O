b7c882947fa33736ac338d45fcf2a664a8479f93
RiyankB807032 <807032@pdsb.net>
2024-10-30 13:40:45 -0400

    Create  Debugdisplaytextvalues.java

---
+++ Debugdisplaytextvalues.java
@@ -0,0 +1,12 @@
+class  Debugdisplaytextvalues {
+  public static void main(String[] args) {
+    // Make any needed corrections to the code below until it runs
+    // without errors.
+
+    String lastMonth = "August";
+
+    System.out.printf("The %s was a great month.\n", lastMonth);
+
+    //////////// End of tasks /////////////////////
+  }
+}
