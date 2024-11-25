95c1712fced5655aec41cc0059a1605a5d6a4d86
RiyankB807032 <807032@pdsb.net>
2024-11-25 12:06:18 -0500

    Create DebugLoopsPartA:debugaconventionalloop.java

---
+++ DebugLoopsPartA:debugaconventionalloop.java
@@ -0,0 +1,14 @@
+import java.lang.Math;
+
+// Fix the errors in the code below to find the first 12
+// numbers and their square roots to 4 decimals.
+
+class DebugLoopsPartA:debugaconventionalloop
+ {
+    public static void main(String [] args) {
+        for (int x = 1; x < 13; x++) {
+            double sr = Math.sqrt(x);
+            System.out.printf("%d %.4f\n", x, sr);
+        }
+    }
+}
